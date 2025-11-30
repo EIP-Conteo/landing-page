6e50f7875f707fb1ca846096a9ad1fcdb93863c5
Theo Fabiano <theo.fabianomattei@gmail.com>
2025-11-30 01:52:40 -0500

    refactor: remove TestimonialsSection from the landing page to streamline content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { HeroSection } from "@/components/sections/HeroSection";
 import { FeaturesSection } from "@/components/sections/FeaturesSection";
 import { HowItWorksSection } from "@/components/sections/HowItWorksSection";
-import { TestimonialsSection } from "@/components/sections/TestimonialsSection";
 import { FAQSection } from "@/components/sections/FAQSection";
 import { Footer } from "@/components/sections/Footer";
 
@@ -11,7 +10,6 @@ export default function Home() {
       <HeroSection />
       <FeaturesSection />
       <HowItWorksSection />
-      <TestimonialsSection />
       <FAQSection />
       <Footer />
     </main>
